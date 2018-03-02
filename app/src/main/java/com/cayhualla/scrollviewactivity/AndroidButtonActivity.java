cca2325c47e020ea77e53a69ab7568610cabd11e
liset08 <liset.cayhualla@tecsup.edu.pe>
2018-03-02 14:58:27 -0500

    Initial commit

---
+++ app/src/main/java/com/cayhualla/scrollviewactivity/AndroidButtonActivity.java
@@ -0,0 +1,17 @@
+package com.cayhualla.scrollviewactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class AndroidButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_button);
+
+
+    }
+
+}
---
+++ app/src/main/java/com/cayhualla/scrollviewactivity/SrollViewActivity.java
@@ -0,0 +1,13 @@
+package com.cayhualla.scrollviewactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SrollViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sroll_view);
+    }
+}
